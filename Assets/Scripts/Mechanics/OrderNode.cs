98dc6da13e32f4ceb614b634736ea7e9190f26e6
allasskkaaaa <lexiecuvin@gmail.com>
2024-11-19 01:57:35 -0500

    more stuff

--- Assets/Scripts/Mechanics/OrderNode.cs
+++ Assets/Scripts/Mechanics/OrderNode.cs
@@ -5,4 +5,18 @@ using UnityEngine;
 public class OrderNode : MonoBehaviour
 {
     [SerializeField] public bool isOccupied = false;
+
+    private void Update()
+    {
+        int childCount = transform.childCount;
+
+        if (childCount > 0 )
+        {
+            isOccupied = true;
+        }
+        else
+        {
+            isOccupied = false;
+        }
+    }
 }
