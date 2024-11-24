332e5c5e223450249a0526f66c3db5289ddac6d7
allasskkaaaa <lexiecuvin@gmail.com>
2024-11-23 22:43:53 -0500

    main menu

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -6,16 +6,52 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
+    private Animator anim;
     [SerializeField] private Button start;
+    [SerializeField] private Button options;
+    [SerializeField] private Button tutorial;
+    [SerializeField] private Button credits;
+    [SerializeField] private Button back;
 
     private void Start()
     {
+        anim = GetComponent<Animator>();
         start.onClick.AddListener(startGame);
+        options.onClick.AddListener(optionsMenu);
+        tutorial.onClick.AddListener(tutorialMenu);
+        credits.onClick.AddListener(creditsMenu);
+        back.onClick.AddListener(backToMenu);
     }
 
-    private void startGame()
+    public void startGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void optionsMenu()
+    {
+        switchPanels("OptionsMenu");
+    }
+
+    public void tutorialMenu()
+    {
+        switchPanels("TutorialMenu");
+    }
+
+    public void creditsMenu()
+    {
+        switchPanels("CreditsMenu");
+    }
+
+    public void backToMenu()
+    {
+        switchPanels("MainMenu");
+    }
+
+    public void switchPanels(string clip)
+    {
+        Debug.Log(clip + " playing");
+        anim.Play(clip);
     }
 
 }
