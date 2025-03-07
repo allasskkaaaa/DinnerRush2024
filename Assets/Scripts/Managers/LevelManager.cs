80fdfcf5906b3a774ceac98103edb82fe213a8a2
allasskkaaaa <lexiecuvin@gmail.com>
2025-03-06 22:41:50 -0500

    refining island 1

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
     [SerializeField] private Inventory foodInventory;
     [SerializeField] private Inventory menuInventory;
     [SerializeField] private FoodObject[] menu = new FoodObject[3];
+    [SerializeField] private GameObject chooseMenuPanel;
+    [SerializeField] private InventoryGrid foodSelection;
 
 
     private void Start()
@@ -17,6 +20,11 @@ public class LevelManager : MonoBehaviour
     private void chooseMenu()
     {
         Time.timeScale = 0;
+        chooseMenuPanel.SetActive(true);
+
+        foodSelection.inventory = foodInventory;
+        foodSelection.GenerateGrid();
+
 
 
     }
